eed18e0d7245448b6c89ef89292aee2882b23528
Doh09 <alexandreasen09@gmail.com>
2020-08-12 20:56:29 +0200

    updating with shop page, collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import HatsPage from './pages/hatspage/hatspage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 import {Route, Switch} from 'react-router-dom';
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <Switch>
     <Route exact='true' path='/' component={HomePage}/>
     <Route exact='true' path='/hats' component={HatsPage}/>
+    <Route exact='true' path='/shop' component={ShopPage}/>
     </Switch>
     </div>
   );
